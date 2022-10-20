f9b99aababc9b76a5f235d489a1077d89166a0af
waterbinnn <ewaterbin@naver.com>
2022-10-20 17:04:34 +0900

    🔨 모바일 네브바 기능구현

--- js/nav.js
+++ js/nav.js
@@ -5,6 +5,9 @@ const companyList = document.querySelector('.menu.item1')
 const artistsList = document.querySelector('.menu.item2')
 const multimediaList = document.querySelector('.menu.item3')
 
+const mobileBtn = document.querySelector('.menu-icon')
+const mobileGnb = document.querySelector('.mobile-menulist')
+
 const navList = [company, artist, multimedia]
 const menu = [companyList, artistsList, multimediaList]
 
@@ -18,7 +21,11 @@ for (let i = 0; i < navList.length; i++) {
   menu[i].addEventListener('mouseleave', () => {
     menu[i].classList.remove('active')
   })
-  navList[i + 1].addEventListener('mouseover', () => {
-    menu[i].classList.remove('active')
-  })
 }
+
+mobileBtn.addEventListener('click', () => {
+  mobileGnb.classList.add('active')
+})
+mobileGnb.addEventListener('mouseleave', () => {
+  mobileGnb.classList.remove('active')
+})
